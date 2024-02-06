8a8b19e516f9a17daf3479d2e2dc0b6e90488969
gracema <gracem2026@student.cis.edu.hk>
2024-02-06 11:22:15 +0800

    done :D

--- src/Address.java
+++ src/Address.java
@@ -1,2 +1,39 @@
 public class Address {
+    // properties
+    private String district;
+    private String street;
+    private int streetNumber;
+
+    // constructor
+    public Address (String dist, String str, int strNum) {
+        district = dist;
+        street = str;
+        streetNumber = strNum;
+    }
+
+    // methods
+    // getters
+    public String getDistrict() {
+        return district;
+    }
+    public String getStreet() {
+        return street;
+    }
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+    // setters
+    public void setDistrict(String d) {
+        district = d;
+    }
+    public void setStreet(String s) {
+        street = s;
+    }
+    public void setStreetNumber(int sN) {
+        streetNumber = sN;
+    }
+    // toString
+    public String toString(){
+        return streetNumber + " " + street + ", " + district;
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,63 @@
 public class Student {
+    // properties
+    private int age;
+    private String firstName;
+    private String lastName;
+    private int year;
+    private Address address;
+
+    // constructor
+    public Student(int a, String fN, String lN, int y, Address ad) {
+        age = a;
+        firstName = fN;
+        lastName = lN;
+        year = y;
+        address = ad;
+    }
+
+    // methods
+    // getters
+    public int getAge() {
+        return age;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public int getYear() {
+        return year;
+    }
+    public Address getAddress() {
+        return address;
+    }
+    // setters
+    public void setAge(int age1) {
+        age = age1;
+    }
+    public void setFirstName(String firstName1) {
+        firstName = firstName1;
+    }
+    public void setLastName(String lastName1) {
+        lastName = lastName1;
+    }
+    public void setYear(int year1) {
+        year = year1;
+    }
+    public void setAddress(Address address1) {
+        address = address1;
+    }
+    // age manipulation
+    public void addOneAge() {
+        age ++;
+    }
+    public int birthday() {
+        addOneAge();
+        return age;
+    }
+    // toString
+    public String toString() {
+        return firstName + " " + lastName + ", age " + age + ", year " + year + ", " + address;
+    }
 }
